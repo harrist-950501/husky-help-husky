009352b13f241de510cbee43bd43901b9740695a
harrist <harrist@uw.edu>
2025-11-16 19:26:55 -0800

    app.js update

--- api.js
+++ api.js
@@ -11,28 +11,6 @@
 
 const express = require("express");
 const app = express();
-// const multer = require("multer");
-// const fs = require("fs").promises;
-
-// app.use(express.urlencoded({extended: true})); // built-in middleware
-// app.use(express.json());
-// app.use(multer().none());
-
-/**
- * Handles server-side errors and responds with a text message.
- * @param {Error} error - the caught server error
- * @param {Response} res - response used to send the error text
- */
-function errorHandling(error, res) {
-  if (error.code === "ENOENT") {
-    res.status(SERVER_SIDE_ERROR).type("text")
-      .send("Server error: file does not exist.");
-  } else {
-    // console.log(error);
-    res.status(SERVER_SIDE_ERROR).type("text")
-      .send("Server error: something went wrong.");
-  }
-}
 
 app.use(express.static("public"));
 const DEFAULT_PORT = 8000;
