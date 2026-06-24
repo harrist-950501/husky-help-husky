e78a1d41115958312fa3da2e67705bc863a83572
harrist <harrist@uw.edu>
2026-06-24 16:23:17 -0700

    Update database schema and ignore seed data

--- table.sql
+++ table.sql
@@ -1,58 +1,48 @@
-CREATE TABLE "sqlite_sequence" (
-	"name"	,
-	"seq"
+CREATE TABLE users (
+	id	SERIAL PRIMARY KEY,
+	username	TEXT NOT NULL UNIQUE,
+	email	TEXT NOT NULL UNIQUE,
+	password	TEXT NOT NULL
 );
 
-CREATE TABLE "users" (
-	"id"	INTEGER,
-	"username"	TEXT NOT NULL UNIQUE,
-	"email"	TEXT NOT NULL UNIQUE,
-	"password"	TEXT NOT NULL,
-	PRIMARY KEY("id" AUTOINCREMENT)
+CREATE TABLE items (
+	id	SERIAL PRIMARY KEY,
+	seller_id	INTEGER NOT NULL,
+	title	TEXT NOT NULL,
+	category	TEXT NOT NULL,
+	description	TEXT,
+	price	REAL NOT NULL,
+	stock	INTEGER NOT NULL,
+	FOREIGN KEY(seller_id) REFERENCES users(id)
 );
 
-CREATE TABLE "items" (
-	"id"	INTEGER,
-	"seller_id"	INTEGER NOT NULL,
-	"title"	TEXT NOT NULL,
-	"category"	TEXT NOT NULL,
-	"description"	TEXT,
-	"price"	REAL NOT NULL,
-	"stock"	INTEGER NOT NULL,
-	PRIMARY KEY("id" AUTOINCREMENT),
-	FOREIGN KEY("seller_id") REFERENCES "users"("id")
+CREATE TABLE transactions (
+	id	SERIAL PRIMARY KEY,
+	confirmation_code	TEXT NOT NULL,
+	buyer_id	INTEGER NOT NULL,
+	seller_id	INTEGER NOT NULL,
+	item_id	INTEGER NOT NULL,
+	date	TIMESTAMP DEFAULT NOW(),
+	FOREIGN KEY(buyer_id) REFERENCES users(id),
+	FOREIGN KEY(item_id) REFERENCES items(id),
+	FOREIGN KEY(seller_id) REFERENCES users(id)
 );
 
-CREATE TABLE "transactions" (
-	"id"	INTEGER,
-	"confirmation_code"	TEXT NOT NULL,
-	"buyer_id"	INTEGER NOT NULL,
-	"seller_id"	INTEGER NOT NULL,
-	"item_id"	INTEGER NOT NULL,
-	"date"	DATETIME DEFAULT (datetime('now', 'localtime')),
-	PRIMARY KEY("id" AUTOINCREMENT),
-	FOREIGN KEY("buyer_id") REFERENCES "users"("id"),
-	FOREIGN KEY("item_id") REFERENCES "items"("id"),
-	FOREIGN KEY("seller_id") REFERENCES "users"("id")
+CREATE TABLE ratings (
+	id	SERIAL PRIMARY KEY,
+	item_id	INTEGER NOT NULL,
+	user_id	INTEGER NOT NULL,
+	stars	INTEGER NOT NULL CHECK(stars BETWEEN 1 AND 5),
+	comment	TEXT,
+	date	TIMESTAMP DEFAULT NOW(),
+	FOREIGN KEY(item_id) REFERENCES items(id),
+	FOREIGN KEY(user_id) REFERENCES users(id)
 );
 
-CREATE TABLE "ratings" (
-	"id"	INTEGER,
-	"item_id"	INTEGER NOT NULL,
-	"user_id"	INTEGER NOT NULL,
-	"stars"	INTEGER NOT NULL CHECK("stars" BETWEEN 1 AND 5),
-	"comment"	TEXT,
-	"date"	DATETIME DEFAULT (datetime('now', 'localtime')),
-	PRIMARY KEY("id" AUTOINCREMENT),
-	FOREIGN KEY("item_id") REFERENCES "items"("id"),
-	FOREIGN KEY("user_id") REFERENCES "users"("id")
-);
-
-CREATE TABLE "user_profiles" (
-	"user_id"	INTEGER,
-	"display_name"	TEXT,
-	"address"	TEXT,
-	"quote"	TEXT,
-	PRIMARY KEY("user_id"),
-	FOREIGN KEY("user_id") REFERENCES "users"("id")
+CREATE TABLE user_profiles (
+	user_id	INTEGER PRIMARY KEY,
+	display_name	TEXT,
+	address	TEXT,
+	quote	TEXT,
+	FOREIGN KEY(user_id) REFERENCES users(id)
 );
